7bcdaa2cdfa51dc5a1ac49c4aa725eb8fa8063fc
locchuong <locchuong123@gmail.com>
2023-11-19 22:03:10 +0700

    update

--- src/textSummarizer/conponents/model_trainer.py
+++ src/textSummarizer/conponents/model_trainer.py
@@ -1,11 +1,10 @@
+import os
+import torch
 from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForSeq2Seq
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 from datasets import load_dataset, load_from_disk
 from textSummarizer.entity import ModelTrainerConfig
-import torch
-import os
-
 
 class ModelTrainer:
     def __init__(self, config: ModelTrainerConfig):
@@ -13,21 +12,34 @@ class ModelTrainer:
 
 
     def train(self):
+        os.environ['CUDA_VISIBLE_DEVICES']='0'
         device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
         model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
         seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
-        
+
+
+        def convert_examples_to_features(example_batch):
+            input_encodings = tokenizer(example_batch['dialogue'],
+                                        max_length=64, #1024
+                                        truncation = True )
+
+            with tokenizer.as_target_tokenizer():
+                target_encodings = tokenizer(
+                    example_batch['summary'],
+                    max_length=64, # 128
+                    truncation=True
+                )
+
+            return{
+                'input_ids': input_encodings['input_ids'],
+                'attention_mask': input_encodings['attention_mask'],
+                'labels': target_encodings['input_ids']
+            }
+
         #loading data 
         dataset_samsum_pt = load_from_disk(self.config.data_path)
-
-        # trainer_args = TrainingArguments(
-        #     output_dir=self.config.root_dir, num_train_epochs=self.config.num_train_epochs, warmup_steps=self.config.warmup_steps,
-        #     per_device_train_batch_size=self.config.per_device_train_batch_size, per_device_eval_batch_size=self.config.per_device_train_batch_size,
-        #     weight_decay=self.config.weight_decay, logging_steps=self.config.logging_steps,
-        #     evaluation_strategy=self.config.evaluation_strategy, eval_steps=self.config.eval_steps, save_steps=1e6,
-        #     gradient_accumulation_steps=self.config.gradient_accumulation_steps
-        # ) 
+        dataset_samsum_pt = dataset_samsum_pt.map(convert_examples_to_features, batched = True)
 
 
         trainer_args = TrainingArguments(
